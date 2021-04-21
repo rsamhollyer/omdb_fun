de900ffab373a05aaf63bd59d296a86172fa2bff
rsamhollyer <rsamhollyer@gmail.com>
2021-04-21 13:58:24 -0500

    fixed the 404 issue wth ratings

--- client/src/components/Ratings.jsx
+++ client/src/components/Ratings.jsx
@@ -1,22 +1,12 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
 function Ratings({ rated }) {
-  const [path, setPath] = useState(rated);
-  useEffect(() => {
-    try {
-      setPath(
-        require(`../assets/images/${rated.replace(" ", "")}.png`).default
-      );
-    } catch (err) {
-      console.log(err);
-      setPath("");
-    }
-  }, [rated]);
-
   return (
     <RatingContainer>
-      <motion.img src={path} alt={rated} />
+      <motion.img
+        src={require(`../assets/images/${rated.replace(" ", "")}.png`).default}
+        alt={rated}
+      />
     </RatingContainer>
   );
 }
